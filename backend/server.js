43533e87d466d29fcb734ae7d30db4f48f8cce01
evfloriz <ev.floriz@gmail.com>
2025-09-02 17:04:32 -0600

    Fix trailing slash

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const server = createServer(app);
 const io = new Server(server, {
     cors: {
         //origin: 'http://localhost:8080',
-        origin: 'https://full-stack-fighter.onrender.com/',
+        origin: 'https://full-stack-fighter.onrender.com',
         methods:['GET', 'POST']
     }
 });
